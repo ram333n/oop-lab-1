167653dfd3ceef0f42ff58d10f1c26b3612a2b3a
Roman Prokopchuk <ghost62727@gmail.com>
2023-05-02 00:27:37 +0300

    🗃️ Define schema (#6)
    * Change port for db
    
    * Add schema.sql

---
+++ airport-management-backend/src/main/resources/schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS crew_members(
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    surname VARCHAR(255) NOT NULL,
+    position VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS flights(
+    id BIGSERIAL PRIMARY KEY,
+    departure_from VARCHAR(255) NOT NULL,
+    destination VARCHAR(255) NOT NULL,
+    departure_time timestamp(6) NOT NULL,
+    arrival_time timestamp(6) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS crew_members_flights(
+    fk_crew_member_id BIGINT REFERENCES crew_members(id) ON DELETE CASCADE ,
+    fk_flight_id BIGINT REFERENCES flights(id) ON DELETE CASCADE
+)
